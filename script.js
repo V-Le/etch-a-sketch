74fe1181d4756d25db931d1ce3b92022d8b798d7
V-Le-D <le.victor@hotmail.com>
2021-07-16 01:04:22 -0500

    added function for grid sizing

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const gridContainer = document.querySelector('.grid-container');
 const blackHover = document.querySelectorAll('.blackHover');
 
-function createGrid() {
-  for (let i = 0; i < 100; i++) {
+function defaultGrid() {
+    createGrid(12);
+}
+
+function createGrid(GridSize) {
+    gridContainer.style.gridTemplateColumns = `repeat(${GridSize}, 1fr)`;
+  for (let i = 0; i < GridSize**2; i++) {
   const divGrid = document.createElement('div');
   divGrid.style.border = '1px red solid';
-  divGrid.classList.add('blackHover');
-  divGrid.addEventListener('mouseover', () => { divGrid.style.backgroundColor = 'black'});
+  //divGrid.classList.add('blackHover');
+  //divGrid.addEventListener('mouseover', () => { divGrid.style.backgroundColor = 'black'});
   gridContainer.appendChild(divGrid);
   }
 }
 
-createGrid();
+defaultGrid();
+
+
+//create prompt for user input
+//create grid column with user input
