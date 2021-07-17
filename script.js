836db7ef25d3f4bb60fa8bb705666b65f7b6d089
V-Le-D <le.victor@hotmail.com>
2021-07-17 10:29:40 -0500

    Added function for user input size

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+const buttonBlack = document.querySelector('#button-black');
+const buttonRainbow = document.querySelector('#button-rainbow');
+const buttonSize = document.querySelector('#button-size');
+const buttonReset = document.querySelector('#button-reset');
+
 const gridContainer = document.querySelector('.grid-container');
 const blackHover = document.querySelectorAll('.blackHover');
 
-function defaultGrid() {
-    createGrid(12);
-}
+buttonSize.addEventListener('click', userInputGrid);
+buttonReset.addEventListener('click', defaultGrid)
 
 function createGrid(GridSize) {
-    gridContainer.style.gridTemplateColumns = `repeat(${GridSize}, 1fr)`;
+  removeAllChildNodes(gridContainer);
+  gridContainer.style.gridTemplateColumns = `repeat(${GridSize}, 1fr)`;
   for (let i = 0; i < GridSize**2; i++) {
-  const divGrid = document.createElement('div');
-  divGrid.style.border = '1px red solid';
-  //divGrid.classList.add('blackHover');
-  //divGrid.addEventListener('mouseover', () => { divGrid.style.backgroundColor = 'black'});
-  gridContainer.appendChild(divGrid);
+    const divGrid = document.createElement('div');
+    divGrid.style.border = '1px red solid';
+    //divGrid.classList.add('blackHover');
+    //divGrid.addEventListener('mouseover', () => { divGrid.style.backgroundColor = 'black'});
+    gridContainer.appendChild(divGrid);
   }
 }
 
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function userInputGrid() {
+  while(isNaN(userGrid) || userGrid > 50 || userGrid < 1) {
+    var userGrid = parseInt(prompt('What size grid between 1-50', 16));
+  }
+  createGrid(userGrid);
+}
+
+function defaultGrid() {
+  createGrid(16);
+}
+
 defaultGrid();
-
-
-//create prompt for user input
-//create grid column with user input
